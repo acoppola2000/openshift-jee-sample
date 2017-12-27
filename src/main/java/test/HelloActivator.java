48e5d2d8bb7d4191398222fa18980dbe5ab76208
A.Coppola <A.Coppola@SVPOS444.S2I.ITA>
2017-12-27 15:52:42 +0100

    test

---
+++ src/main/java/test/HelloActivator.java
@@ -0,0 +1,14 @@
+package main.java.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Used to bootstrap JAX-RS.  Otherwise this class is
+ * not directly used.
+ *
+ */
+@ApplicationPath("/rest")
+public class HelloActivator extends Application {
+    // intentionally empty
+}
---
+++ src/main/java/test/HelloService.java
@@ -0,0 +1,34 @@
+package main.java.test;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
+ * is enabled
+ *
+ * @author gbrey@redhat.com
+ *
+ */
+
+@Path("/")
+public class HelloWorld {
+
+
+    @GET
+    @Path("/json")
+    @Produces({ "application/json" })
+    public String getHelloWorldJSON() {
+        return "{\"result\":\"" + "Hello World" + "\"}";
+    }
+
+    @GET
+    @Path("/xml")
+    @Produces({ "application/xml" })
+    public String getHelloWorldXML() {
+        return "<xml><result>" + "Hello World" + "</result></xml>";
+    }
+
+}
